88fb9ff63689904ce5036f709188bd8e7ba4678a
Admin <sidagulov@gmail.com>
2024-02-13 23:21:59 +0200

    serik 1

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,6 +2,12 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+
         System.out.println("Bug Fighters");
+
+        System.out.println("Demet, simdi calisiyor proje");
+
     }
+
+
 }
